356a978fcff6e2ebd50acbfe13b8982bf7982f82
Lewis Westbury <lewis@cantab.net>
2022-04-03 03:31:58 +0100

    rebuild of app Sun Apr  3 03:31:58 BST 2022

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -2,356 +2,356 @@
   "assets": [
     {
       "hash": "sha256-SO\/fuIIWUxoIu1OABrOpK5n\/AvzxJcg+0H3PT1eutAE=",
-      "url": "our-wedding\/css\/app.css"
+      "url": "css\/app.css"
     },
     {
       "hash": "sha256-DWuUK8Wq1I48xISPmbZWph3Z9X8SpOCAnVYGG2B7YDA=",
-      "url": "our-wedding\/css\/bootstrap\/bootstrap.min.css"
+      "url": "css\/bootstrap\/bootstrap.min.css"
     },
     {
       "hash": "sha256-5LMtsmhqeZknQQHS0Q4BoUxOgzV2IWZOkWH8CPlC+0Y=",
-      "url": "our-wedding\/css\/bootstrap\/bootstrap.min.css.map"
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
     },
     {
       "hash": "sha256-+7Zqv8jozZ12rlwIIfxUcaULb2pPImVOydwizJZ3W9Y=",
-      "url": "our-wedding\/css\/open-iconic\/FONT-LICENSE"
+      "url": "css\/open-iconic\/FONT-LICENSE"
     },
     {
       "hash": "sha256-VKmQXW8mRZ7hoad6E\/sXRxRWnDwfp6ihgfNDt3M7q9Q=",
-      "url": "our-wedding\/css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
     },
     {
       "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
-      "url": "our-wedding\/css\/open-iconic\/font\/fonts\/open-iconic.eot"
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
     },
     {
       "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
-      "url": "our-wedding\/css\/open-iconic\/font\/fonts\/open-iconic.otf"
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
     },
     {
       "hash": "sha256-XNMzPK4pV4aBXs1ZjC4JlJZWZ84qhOBD6+86ea79a5E=",
-      "url": "our-wedding\/css\/open-iconic\/font\/fonts\/open-iconic.svg"
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
     },
     {
       "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
-      "url": "our-wedding\/css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
     },
     {
       "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
-      "url": "our-wedding\/css\/open-iconic\/font\/fonts\/open-iconic.woff"
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
     },
     {
       "hash": "sha256-lmR0kygUnUAknae38gH2rAsdPtwBPv4tfDUB8r3vMes=",
-      "url": "our-wedding\/css\/open-iconic\/ICON-LICENSE"
+      "url": "css\/open-iconic\/ICON-LICENSE"
     },
     {
       "hash": "sha256-0udU+rWVlUsIZ2hAiFjYKIBc5hwpgpSZ6pq3aqcAtsY=",
-      "url": "our-wedding\/css\/open-iconic\/README.md"
+      "url": "css\/open-iconic\/README.md"
     },
     {
       "hash": "sha256-Jtxf9L+5ITKRc1gIRl4VbUpGkRNfOBXjYTdhJD4facM=",
-      "url": "our-wedding\/favicon.ico"
+      "url": "favicon.ico"
     },
     {
       "hash": "sha256-IpHFcQvs03lh6W54zoeJRG1jW+VnGmwymzxLoXtKX7Y=",
-      "url": "our-wedding\/icon-512.png"
+      "url": "icon-512.png"
     },
     {
-      "hash": "sha256-Dh+tOh0E2liBXqpVNAZCdFC1y81Fa+yn16haEe0dt0o=",
-      "url": "our-wedding\/index.html"
+      "hash": "sha256-2GrxyziUQ42Z\/qNrNPJ8P719+bjgg3EAEpY132NJwSk=",
+      "url": "index.html"
     },
     {
       "hash": "sha256-w0CzXNDeRGjb6ICpgQvRZxJ3SbvRz9sN1cc4SHL3nB0=",
-      "url": "our-wedding\/manifest.json"
+      "url": "manifest.json"
     },
     {
       "hash": "sha256-4Yh4OTT5TEAPNyXEzOG+n5l2HPK294WFItz4SIelj88=",
-      "url": "our-wedding\/sample-data\/weather.json"
+      "url": "sample-data\/weather.json"
     },
     {
       "hash": "sha256-ootDKSA6cNAbgJLmPhrHsPVvVqkGEYdRZxucC22LWjw=",
-      "url": "our-wedding\/_framework\/dotnet.timezones.blat"
+      "url": "_framework\/dotnet.timezones.blat"
     },
     {
       "hash": "sha256-YXYNlLeMqRPFVpY2KSDhleLkNk35d9KvzzwwKAoiftc=",
-      "url": "our-wedding\/_framework\/dotnet.wasm"
+      "url": "_framework\/dotnet.wasm"
     },
     {
       "hash": "sha256-m7NyeXyxM+CL04jr9ui1Z6pVfMWwhHusuz5qNZWpAwA=",
-      "url": "our-wedding\/_framework\/icudt.dat"
+      "url": "_framework\/icudt.dat"
     },
     {
       "hash": "sha256-91bygK5voY9lG5wxP0\/uj7uH5xljF9u7iWnSldT1Z\/g=",
-      "url": "our-wedding\/_framework\/icudt_CJK.dat"
+      "url": "_framework\/icudt_CJK.dat"
     },
     {
       "hash": "sha256-DPfeOLph83b2rdx40cKxIBcfVZ8abTWAFq+RBQMxGw0=",
-      "url": "our-wedding\/_framework\/icudt_EFIGS.dat"
+      "url": "_framework\/icudt_EFIGS.dat"
     },
     {
       "hash": "sha256-oM7Z6aN9jHmCYqDMCBwFgFAYAGgsH1jLC\/Z6DYeVmmk=",
-      "url": "our-wedding\/_framework\/icudt_no_CJK.dat"
+      "url": "_framework\/icudt_no_CJK.dat"
     },
     {
       "hash": "sha256-lg8Ao7vtV0csLoUmg4UFKwQPAk9H1pbzQBbcF9D42vg=",
-      "url": "our-wedding\/_framework\/dotnet.5.0.15.js"
+      "url": "_framework\/dotnet.5.0.15.js"
     },
     {
-      "hash": "sha256-KE\/i7yajDPRsosSdQQe3340BGfwdLnTyHnH0ToWzjQg=",
-      "url": "our-wedding\/our-wedding\/WeddingApp.UI.styles.css"
+      "hash": "sha256-ptdX6wKNiWOmCnKTs\/JvKgc4xjCWU1niDwrzKs4FLNE=",
+      "url": "WeddingApp.UI.styles.css"
     },
     {
       "hash": "sha256-mfUPOnPJdVlqx5sfEyjLXvZojwvosT7IdwftBM2ekfA=",
-      "url": "our-wedding\/_framework\/Blazored.LocalStorage.dll"
+      "url": "_framework\/Blazored.LocalStorage.dll"
     },
     {
       "hash": "sha256-NsX0JTLefBulFJuOOQ7a5NES1\/0JroLi6pnAfNlYW04=",
-      "url": "our-wedding\/_framework\/CsvHelper.dll"
+      "url": "_framework\/CsvHelper.dll"
     },
     {
       "hash": "sha256-JygwxMGKOcbJEYXUwANHMVPLT9Z0\/MAOQLdTbrHNZN4=",
-      "url": "our-wedding\/_framework\/Microsoft.AspNetCore.Authorization.dll"
+      "url": "_framework\/Microsoft.AspNetCore.Authorization.dll"
     },
     {
       "hash": "sha256-EN5mRk87YHWVnqCQT9cMlQ3vUeNoGI2N9U5Sjq+rdXc=",
-      "url": "our-wedding\/_framework\/Microsoft.AspNetCore.Components.dll"
+      "url": "_framework\/Microsoft.AspNetCore.Components.dll"
     },
     {
       "hash": "sha256-EU0Harm26x1zF63oREwu64aQJZ+vxZow+ZzU\/nqYsBg=",
-      "url": "our-wedding\/_framework\/Microsoft.AspNetCore.Components.Web.dll"
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.dll"
     },
     {
       "hash": "sha256-+TXCmkbaJb4qGWVV0+V8LecStDs7UBsM5E0g2+PPT5M=",
-      "url": "our-wedding\/_framework\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.dll"
     },
     {
       "hash": "sha256-ac4stn1eobk9p\/24HUlvMqxpBymazpNVPsU3i5FCNKM=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Configuration.dll"
+      "url": "_framework\/Microsoft.Extensions.Configuration.dll"
     },
     {
       "hash": "sha256-BJeRgbVSljLQ+sCKsDWwFbyJvkSF84fed7rcf5+GK4s=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Configuration.Abstractions.dll"
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.dll"
     },
     {
       "hash": "sha256-GcqOItT9TLap0aRlf66Tqc0ycCSRvh9ZZ2eP2AnyT90=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Configuration.Json.dll"
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.dll"
     },
     {
       "hash": "sha256-sgCEILhEe6WfeIiyU8NxB9YcVy\/t1tX5IxeaoopTESQ=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.DependencyInjection.dll"
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.dll"
     },
     {
       "hash": "sha256-Ak0qluJwuubseEt5fXcb09kzaGAiZFFW3j6AVQj5LaU=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
     },
     {
       "hash": "sha256-VtXlw8ZzXr\/uJukIxl2amVlI0iX5AudhX8bQF2k1mjI=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Logging.dll"
+      "url": "_framework\/Microsoft.Extensions.Logging.dll"
     },
     {
       "hash": "sha256-JVBNiak\/8B3AcQkWEGr3wUXTGCHLabZtaMFtFk71jtE=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Logging.Abstractions.dll"
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.dll"
     },
     {
       "hash": "sha256-BYpZxOM\/5gKKZ5I9Kh6EFcbuGdPYK3zRm4SONJ0ISFg=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Options.dll"
+      "url": "_framework\/Microsoft.Extensions.Options.dll"
     },
     {
       "hash": "sha256-UQLLfErWJmykNU\/8fVl1tpNwAVaDmX6UFcBr2P2SgMk=",
-      "url": "our-wedding\/_framework\/Microsoft.Extensions.Primitives.dll"
+      "url": "_framework\/Microsoft.Extensions.Primitives.dll"
     },
     {
       "hash": "sha256-lRltBpsJTXW9P43UvwnTc\/\/08+GI\/C70tkuJaUjJMv0=",
-      "url": "our-wedding\/_framework\/Microsoft.JSInterop.dll"
+      "url": "_framework\/Microsoft.JSInterop.dll"
     },
     {
       "hash": "sha256-IjG42rZjbdAnOMJMlqcJW3tLJh2lwkStSndPy7zjZV4=",
-      "url": "our-wedding\/_framework\/Microsoft.JSInterop.WebAssembly.dll"
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.dll"
     },
     {
       "hash": "sha256-\/C5xhI+m3Aqrcjxy\/TauLyqv\/\/5l8WEW3rXFtVlNhy8=",
-      "url": "our-wedding\/_framework\/System.IO.Pipelines.dll"
+      "url": "_framework\/System.IO.Pipelines.dll"
     },
     {
       "hash": "sha256-E9qfg1RKwNdFu8IKZmzVmQJF8+fLGE5jCNkQH4lsvgE=",
-      "url": "our-wedding\/_framework\/WeddingApp.UI.dll"
+      "url": "_framework\/WeddingApp.UI.dll"
     },
     {
       "hash": "sha256-A10B60k6+KNJC+a4VzvN\/MXhMJwPGrSuVYse7gsPJ0o=",
-      "url": "our-wedding\/_framework\/Microsoft.CSharp.dll"
+      "url": "_framework\/Microsoft.CSharp.dll"
     },
     {
       "hash": "sha256-1qYO4EDsiN069HAXNgAjexWrvcDMM71YlBMts1AQJ5E=",
-      "url": "our-wedding\/_framework\/System.Buffers.dll"
+      "url": "_framework\/System.Buffers.dll"
     },
     {
       "hash": "sha256-CQHcUw0xg7QoUFuLTYfp8CSxCrTRfZM53ApzAd45kUc=",
-      "url": "our-wedding\/_framework\/System.Collections.Concurrent.dll"
+      "url": "_framework\/System.Collections.Concurrent.dll"
     },
     {
       "hash": "sha256-KTxX9ZN3eJUFaMt4irBkbywA0RKCsOJQHDb3SWO34kk=",
-      "url": "our-wedding\/_framework\/System.Collections.Immutable.dll"
+      "url": "_framework\/System.Collections.Immutable.dll"
     },
     {
       "hash": "sha256-03DckkzVdedQS4A7964kXvus+qm48CT+sR0Orp1\/Ctk=",
-      "url": "our-wedding\/_framework\/System.Collections.NonGeneric.dll"
+      "url": "_framework\/System.Collections.NonGeneric.dll"
     },
     {
       "hash": "sha256-0UL06rVoRzxuLpBAN49QWCWfjV\/o4Uy7gIZPsTO\/pu4=",
-      "url": "our-wedding\/_framework\/System.Collections.Specialized.dll"
+      "url": "_framework\/System.Collections.Specialized.dll"
     },
     {
       "hash": "sha256-MPZFEOf6lyz8bsgnVmeUeSLau1q3H9YR\/mu1FUSwfR0=",
-      "url": "our-wedding\/_framework\/System.Collections.dll"
+      "url": "_framework\/System.Collections.dll"
     },
     {
       "hash": "sha256-c0mfA\/8j7dyJQgEM5ORJ8Aa438Aw7S709UxI3pL3qcY=",
-      "url": "our-wedding\/_framework\/System.ComponentModel.Primitives.dll"
+      "url": "_framework\/System.ComponentModel.Primitives.dll"
     },
     {
       "hash": "sha256-nPv20SPBvEO5IJJKSfCG6lA+c0dvx9alUYbPzG2UQ7Q=",
-      "url": "our-wedding\/_framework\/System.ComponentModel.TypeConverter.dll"
+      "url": "_framework\/System.ComponentModel.TypeConverter.dll"
     },
     {
       "hash": "sha256-DpsDhUgeaGsE8NGFu8BNY9VjWPvOasVLHblAA66VRdU=",
-      "url": "our-wedding\/_framework\/System.ComponentModel.dll"
+      "url": "_framework\/System.ComponentModel.dll"
     },
     {
       "hash": "sha256-ghY7j+yJ4yQbhTPfrpphy\/Bs8NHhBUZ2kQAD\/8+gDsM=",
-      "url": "our-wedding\/_framework\/System.Console.dll"
+      "url": "_framework\/System.Console.dll"
     },
     {
       "hash": "sha256-QZ0JBtgSU9ZvMjTlfIyh+xdTBlt0Me\/qPbW\/9xKt0pY=",
-      "url": "our-wedding\/_framework\/System.Data.Common.dll"
+      "url": "_framework\/System.Data.Common.dll"
     },
     {
       "hash": "sha256-27vX\/6OA1yDtrwc71toUxnhayDIql+hS27fvkP2\/JFE=",
-      "url": "our-wedding\/_framework\/System.Diagnostics.Debug.dll"
+      "url": "_framework\/System.Diagnostics.Debug.dll"
     },
     {
       "hash": "sha256-9B3HKBUxyjuYEOHU+EXgjrEynGbdAXW1k8szWdZzVRU=",
-      "url": "our-wedding\/_framework\/System.Diagnostics.TraceSource.dll"
+      "url": "_framework\/System.Diagnostics.TraceSource.dll"
     },
     {
       "hash": "sha256-b\/LkTHFK7vQO8Kd+ee0YzNBkc695CZfLw8FF5mCTvy0=",
-      "url": "our-wedding\/_framework\/System.Drawing.Primitives.dll"
+      "url": "_framework\/System.Drawing.Primitives.dll"
     },
     {
       "hash": "sha256-3LPfBSocn40o2ZY43vRdL9vm\/\/yYXoZT6dVU8eGsSxA=",
-      "url": "our-wedding\/_framework\/System.Drawing.dll"
+      "url": "_framework\/System.Drawing.dll"
     },
     {
       "hash": "sha256-0Hdv\/LEyQwBH\/qRcRPtzUwffjJVZiqL6M+epesxS8qc=",
-      "url": "our-wedding\/_framework\/System.IO.FileSystem.dll"
+      "url": "_framework\/System.IO.FileSystem.dll"
     },
     {
       "hash": "sha256-sVXBaQ\/VQj8pnSpzVaJ2Tpl89eUAjbtJ6OiU4mirLuY=",
-      "url": "our-wedding\/_framework\/System.Linq.Expressions.dll"
+      "url": "_framework\/System.Linq.Expressions.dll"
     },
     {
       "hash": "sha256-O6e2Igw5BeOi\/VeBa1Xs3UTgMSvGoD+TIPwE6UgaDsc=",
-      "url": "our-wedding\/_framework\/System.Linq.dll"
+      "url": "_framework\/System.Linq.dll"
     },
     {
       "hash": "sha256-SfT7ul3ny931OYIz2x8TTp5FHqb\/FJ65TzJDNGnSge8=",
-      "url": "our-wedding\/_framework\/System.Memory.dll"
+      "url": "_framework\/System.Memory.dll"
     },
     {
       "hash": "sha256-cBosbdb13gebA1w49gk8597L4tHAQJ3P19pi9AOQjKU=",
-      "url": "our-wedding\/_framework\/System.Net.Http.dll"
+      "url": "_framework\/System.Net.Http.dll"
     },
     {
       "hash": "sha256-RSENdSo668xcFXRl\/2iC6PnZQl\/uyTb5BybF7wXGoRQ=",
-      "url": "our-wedding\/_framework\/System.Net.Primitives.dll"
+      "url": "_framework\/System.Net.Primitives.dll"
     },
     {
       "hash": "sha256-d71fnnJRdKW3iW+vm7Ii3DfoNMAIzd6s72xW8KHwVGA=",
-      "url": "our-wedding\/_framework\/System.ObjectModel.dll"
+      "url": "_framework\/System.ObjectModel.dll"
     },
     {
       "hash": "sha256-bVgGPUZFh50elwmiSpEDISzt1H6PWX4KzkofpcKg734=",
-      "url": "our-wedding\/_framework\/System.Private.Runtime.InteropServices.JavaScript.dll"
+      "url": "_framework\/System.Private.Runtime.InteropServices.JavaScript.dll"
     },
     {
       "hash": "sha256-GtZ8PBkxbKmkmRbTwhyVNrMqo1JuGdMKLdtPkk1gn5w=",
-      "url": "our-wedding\/_framework\/System.Private.Uri.dll"
+      "url": "_framework\/System.Private.Uri.dll"
     },
     {
       "hash": "sha256-XUwWCwndubl5qPJ6jktOvjaQ0vH38XECk+fhRYJNwKM=",
-      "url": "our-wedding\/_framework\/System.Private.Xml.dll"
+      "url": "_framework\/System.Private.Xml.dll"
     },
     {
       "hash": "sha256-+vcVEexFKTs0I3A2oJrS5e\/FcnsBsuAQG36EkNWF7Rk=",
-      "url": "our-wedding\/_framework\/System.Resources.ResourceManager.dll"
+      "url": "_framework\/System.Resources.ResourceManager.dll"
     },
     {
       "hash": "sha256-0UCHWHJrs09ayoq8TpflyePtjQ6RRLn5kL1YTeAK5zU=",
-      "url": "our-wedding\/_framework\/System.Runtime.CompilerServices.Unsafe.dll"
+      "url": "_framework\/System.Runtime.CompilerServices.Unsafe.dll"
     },
     {
       "hash": "sha256-N4kHfZWGcMztcmMtwMM1cV4ePOhAyhxXwQD3Bbzl0Co=",
-      "url": "our-wedding\/_framework\/System.Runtime.Extensions.dll"
+      "url": "_framework\/System.Runtime.Extensions.dll"
     },
     {
       "hash": "sha256-bZTUrwS5C4TRZnuydFDLfTyxgcpSautkHbBKzRPHm3w=",
-      "url": "our-wedding\/_framework\/System.Runtime.InteropServices.RuntimeInformation.dll"
+      "url": "_framework\/System.Runtime.InteropServices.RuntimeInformation.dll"
     },
     {
       "hash": "sha256-HDjbVwva\/N2WqEdc7u2EojXCnx0R3+PLAwaRnnSiTes=",
-      "url": "our-wedding\/_framework\/System.Runtime.Numerics.dll"
+      "url": "_framework\/System.Runtime.Numerics.dll"
     },
     {
       "hash": "sha256-2pJ0VaMhPIAKsZyLQUtbZadejWJ\/3oanH0vVWOUPxjo=",
-      "url": "our-wedding\/_framework\/System.Runtime.Serialization.Formatters.dll"
+      "url": "_framework\/System.Runtime.Serialization.Formatters.dll"
     },
     {
       "hash": "sha256-5q85mxVevNvqXVnApPOKq+Q6CJhjKuM5vp6AaitNpfY=",
-      "url": "our-wedding\/_framework\/System.Runtime.dll"
+      "url": "_framework\/System.Runtime.dll"
     },
     {
       "hash": "sha256-u7g04O1\/XzZr0I4G8zSRyr9hKxZ2EH6NPTRBiKPsxDA=",
-      "url": "our-wedding\/_framework\/System.Security.Cryptography.Algorithms.dll"
+      "url": "_framework\/System.Security.Cryptography.Algorithms.dll"
     },
     {
       "hash": "sha256-OQelb4EN9OPbHiSYsjbSeK810QOCaXsX6PqOu\/WCnA8=",
-      "url": "our-wedding\/_framework\/System.Text.Encodings.Web.dll"
+      "url": "_framework\/System.Text.Encodings.Web.dll"
     },
     {
       "hash": "sha256-3+PSEWjgF8waYcwRGvU6OJHvOjTOnRDAKmJXIjjhoKU=",
-      "url": "our-wedding\/_framework\/System.Text.Json.dll"
+      "url": "_framework\/System.Text.Json.dll"
     },
     {
       "hash": "sha256-XhoaN5wf84FimK4CT+IjheZN944doZ15un6zcbonMqM=",
-      "url": "our-wedding\/_framework\/System.Text.RegularExpressions.dll"
+      "url": "_framework\/System.Text.RegularExpressions.dll"
     },
     {
       "hash": "sha256-K+ZngyWkSLmxLXGAW0EgI7UTyRK2uGFIU3mGfqdOye4=",
-      "url": "our-wedding\/_framework\/System.Threading.Tasks.dll"
+      "url": "_framework\/System.Threading.Tasks.dll"
     },
     {
       "hash": "sha256-tqfg+mKb8+BfHOzfAjjKxcmbNXogCeZ0vzsEoO1GZPw=",
-      "url": "our-wedding\/_framework\/System.Threading.ThreadPool.dll"
+      "url": "_framework\/System.Threading.ThreadPool.dll"
     },
     {
       "hash": "sha256-zBMz2DZSDtD4GIvF0gmUs0LN8jqEn03WNZC\/9qW1vpM=",
-      "url": "our-wedding\/_framework\/System.Threading.dll"
+      "url": "_framework\/System.Threading.dll"
     },
     {
       "hash": "sha256-g9qj1t6HoxgsyyM7RSVoeO8igpGGLsc5w7p\/cET3aqs=",
-      "url": "our-wedding\/_framework\/System.dll"
+      "url": "_framework\/System.dll"
     },
     {
       "hash": "sha256-taVvieSyQPBOGSmOY7nbBJg39pzBUuBn15LWjkHtIu4=",
-      "url": "our-wedding\/_framework\/System.Private.CoreLib.dll"
+      "url": "_framework\/System.Private.CoreLib.dll"
     },
     {
       "hash": "sha256-fS1nzHGc6Z3ahloNc4tGdY+pEx9KZEZUWHEGv91KcDE=",
-      "url": "our-wedding\/_framework\/blazor.boot.json"
+      "url": "_framework\/blazor.boot.json"
     },
     {
       "hash": "sha256-dl8FVRvWOJfOtzIC\/x66QNnBNsT9cAOMrn22GB8fJ8U=",
-      "url": "our-wedding\/_framework\/blazor.webassembly.js"
+      "url": "_framework\/blazor.webassembly.js"
     }
   ],
-  "version": "LP3G+y2k"
+  "version": "ZZE7R4DI"
 };
