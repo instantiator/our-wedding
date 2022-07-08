ef04998143d9985040e9ab4b815e6321b94d9777
Lewis Westbury <lewis@cantab.net>
2022-07-08 19:09:06 +0100

    rebuild of app Fri Jul  8 19:09:06 BST 2022

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -321,7 +321,7 @@
       "url": "icon-512.png"
     },
     {
-      "hash": "sha256-x3jJUORuX0FWNS8hCU27AnX4NuvRQjzjosITjQVumzc=",
+      "hash": "sha256-Al5TWjM4Yhkwh6FR\/mvFO4QSegwfTY1fAPU0STLy6mo=",
       "url": "index.html"
     },
     {
@@ -337,5 +337,5 @@
       "url": "sample-data\/default-variables.csv"
     }
   ],
-  "version": "F1qgoXmP"
+  "version": "OkpE9+2w"
 };
--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
-const CACHE_VERSION = '2022.07.08-004';
+const CACHE_VERSION = '2022.07.08-005';
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}-${CACHE_VERSION}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
@@ -47,4 +47,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: F1qgoXmP */
+/* Manifest version: OkpE9+2w */
